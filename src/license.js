36c363e79691dd73d58323d9ff080d69087f55df
FantasticFiasco <mattias.kindborg@gmail.com>
2019-12-29 09:22:12 +0100

    feat: documentation and logging

--- src/license.js
+++ src/license.js
@@ -1,22 +1,38 @@
+const core = require("@actions/core");
 const fs = require("fs");
 
+// Regular expressions finding the copyright year(s) in Apache 2.0 license files
 const copyrightYear = /(Copyright )(\d{4})( \w+)/gm;
 const copyrightYearRange = /(Copyright )(\d{4})-(\d{4})( \w+)/gm;
 
+/**
+ * Update the copyright year(s) in a license file named LICENSE in the root of the repository.
+ *
+ * @returns {String}        The updated content of the license file.
+ */
 function update() {
     const fileName = "LICENSE";
     const newYear = new Date().getFullYear();
     return updateFile(fileName, newYear);
 }
 
+/**
+ * Update the copyright year(s) in specified license file.
+ *
+ * @param {String} fileName The license file name.
+ * @param {Number} year     The year the license is valid to.
+ * @returns {String}        The updated content of the license file.
+ */
 function updateFile(fileName, year) {
+    core.info(`Update year to ${year} in licese file ${fileName}`);
+
     let license = readLicenseFile(fileName);
 
     if (copyrightYear.test(license)) {
-        // Turn a single year into a range of years
+        core.info("Updating from a single year into a range of years");
         license = license.replace(copyrightYear, `$1$2-${year}$3`);
     } else if (copyrightYearRange.test(license)) {
-        // Update the range of years
+        core.info("Updating range of years");
         license = license.replace(copyrightYearRange, `$1$2-${year}$4`);
     } else {
         throw new Error(`Format of license file ${fileName} is not supported`);
@@ -28,10 +44,12 @@ function updateFile(fileName, year) {
 }
 
 function readLicenseFile(fileName) {
+    core.info(`Read content of file ${fileName}`);
     return fs.readFileSync(fileName).toString();
 }
 
 function writeLicenseFile(fileName, license) {
+    core.info(`Write content to file ${fileName}`);
     fs.writeFileSync(fileName, license);
 }
 
