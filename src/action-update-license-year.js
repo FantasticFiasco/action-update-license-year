73634122a31080963dfa4d09824cf75debd35eee
FantasticFiasco <mattias.kindborg@gmail.com>
2020-07-05 17:31:18 +0200

    wip

--- src/action-update-license-year.js
+++ src/action-update-license-year.js
@@ -3,6 +3,9 @@ import { context } from '@actions/github';
 import { GitHub } from './github';
 import { License } from './license';
 
+const FILENAME = 'LICENSE';
+const BRANCH_NAME = `license/copyright-to-${new Date().getFullYear()}`;
+
 export const run = async () => {
     try {
         // Context
@@ -11,16 +14,16 @@ export const run = async () => {
         // Inputs
         const token = getInput('token', { required: true });
 
-        const github = new GitHub(token, owner, repo);
+        const github = new GitHub(owner, repo, token);
 
-        await github.createBranch();
-        const res = await github.readLicenseFile();
+        await github.createBranch(BRANCH_NAME);
+        const res = await github.getContent('master', FILENAME);
         const currentLicense = Buffer.from(res.data.content, 'base64').toString('ascii');
 
         const license = new License();
         const updatedLicense = license.update(currentLicense);
-        await github.writeLicenseFile(res.data.sha, updatedLicense);
-        await github.createPullRequest();
+        await github.updateContent(BRANCH_NAME, FILENAME, res.data.sha, updatedLicense);
+        await github.createPullRequest(BRANCH_NAME);
     } catch (err) {
         setFailed(err.message);
     }
