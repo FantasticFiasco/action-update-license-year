581b65f5fab9ebdc9b1fd126e4c843fa0b57473f
FantasticFiasco <mattias.kindborg@gmail.com>
2020-07-10 20:20:46 +0200

    wip

--- test/action-update-license-year.spec.js
+++ test/action-update-license-year.spec.js
@@ -39,10 +39,12 @@ jest.mock('../src/license', () => ({
 describe('running action should', () => {
     test("create PR with updated license given branch doesn't exist", async () => {
         const octokit = getOctokit('some token');
-        mockBranch(octokit, false);
-        mockGetContent(octokit);
-        mockUpdateLicense(octokit);
-        mockCreateBranch(octokit);
+        octokit.git.getRef.mockImplementation((params) => {
+            return params.ref === 'heads/master' ? res.getRef.success : res.getRef.failure;
+        });
+        octokit.repos.getContent.mockResolvedValue(res.getContent.success);
+        updateLicense.mockReturnValue('some updated license');
+        octokit.git.createRef.mockResolvedValue();
         octokit.pulls.list.mockResolvedValue({ data: [] });
         octokit.pulls.create.mockResolvedValue({});
         await run();
@@ -53,40 +55,25 @@ describe('running action should', () => {
     });
 });
 
-/**
- * @param {boolean} exists
- */
-const mockBranch = (octokit, exists) => {
-    octokit.git.getRef.mockImplementation((params) => {
-        if (exists || params.ref === 'heads/master') {
-            return Promise.resolve({
-                status: 200,
-                data: {
-                    object: {
-                        sha: 'some sha',
-                    },
+const res = {
+    getRef: {
+        success: Promise.resolve({
+            status: 200,
+            data: {
+                object: {
+                    sha: 'some sha',
                 },
-            });
-        }
-
-        return Promise.reject({
+            },
+        }),
+        failure: Promise.reject({
             status: 404,
-        });
-    });
-};
-
-const mockGetContent = (octokit) => {
-    octokit.repos.getContent.mockResolvedValue({
-        data: {
-            content: Buffer.from('some license').toString('base64'),
+        }),
+    },
+    getContent: {
+        success: {
+            data: {
+                content: Buffer.from('some license').toString('base64'),
+            },
         },
-    });
-};
-
-const mockUpdateLicense = () => {
-    updateLicense.mockReturnValue('some updated license');
-};
-
-const mockCreateBranch = (octokit) => {
-    octokit.git.createRef.mockResolvedValue();
+    },
 };
