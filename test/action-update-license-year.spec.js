02f1e1c77e6ccf9df067b5b99b608e089d4ae4e4
FantasticFiasco <mattias.kindborg@gmail.com>
2020-07-11 23:09:53 +0200

    wip

--- test/action-update-license-year.spec.js
+++ test/action-update-license-year.spec.js
@@ -1,22 +1,47 @@
+const mockRepository = {
+    hasBranch: jest.fn(),
+    createBranch: jest.fn(),
+    getContent: jest.fn(),
+    updateContent: jest.fn(),
+    hasPullRequest: jest.fn(),
+    createPullRequest: jest.fn(),
+};
+
+jest.mock('@actions/core');
+jest.mock('@actions/github');
+jest.mock('../src/license');
+jest.mock('../src/Repository', () => {
+    return jest.fn().mockImplementation(() => {
+        return mockRepository;
+    });
+});
+
 const { run } = require('../src/action-update-license-year');
+const { getInput, setFailed } = require('@actions/core');
+const { context } = require('@actions/github');
+const { updateLicense } = require('../src/license');
 
 describe('running action should', () => {
-    test("create PR with updated license given branch doesn't exist", async () => {
-        // Repository.mock.
+    beforeEach(() => {
+        context.repo = {
+            owner: 'FantasticFiasco',
+            repo: 'action-update-license-year',
+        };
+    });
 
-        // const octokit = getOctokit('some token');
-        // octokit.git.getRef.mockImplementation((params) => {
-        //     return params.ref === 'heads/master' ? res.git.getRef.success : res.git.getRef.failure;
-        // });
-        // octokit.repos.getContent.mockResolvedValue(res.git.getContent.success);
-        // updateLicense.mockReturnValue('some updated license');
-        // octokit.git.createRef.mockResolvedValue();
-        // octokit.pulls.list.mockResolvedValue(res.pulls.list.notEmpty);
-        // octokit.pulls.create.mockResolvedValue({});
+    test("create PR with updated license given branch doesn't exist", async () => {
+        getInput.mockReturnValue('some token');
+        mockRepository.hasBranch.mockReturnValue(false);
+        mockRepository.getContent.mockReturnValue({
+            data: {
+                content: Buffer.from('some license').toString('base64'),
+            },
+        });
+        updateLicense.mockReturnValue('some updated license');
         await run();
-        // expect(octokit.git.createRef).toBeCalledTimes(1);
-        // expect(octokit.repos.createOrUpdateFileContents).toBeCalledTimes(1);
-        // expect(octokit.pulls.create).toBeCalledTimes(1);
-        // expect(setFailed).toBeCalledTimes(0);
+        expect(mockRepository.createBranch).toBeCalledTimes(1);
+        expect(mockRepository.updateContent).toBeCalledTimes(1);
+        expect(mockRepository.createPullRequest).toBeCalledTimes(1);
+        expect(setFailed).toBeCalledTimes(0);
     });
 });
