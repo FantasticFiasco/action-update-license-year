17980e04c102368fb03214c5ea33935ffac1fcbe
FantasticFiasco <mattias.kindborg@gmail.com>
2020-07-14 08:49:40 +0200

    wip

--- src/Repository.js
+++ src/Repository.js
@@ -85,24 +85,24 @@ class Repository {
 
     /**
      * @param {string} branchName The name of the branch
-     * @param {string} path The file path
+     * @param {string} filePath The file path
      * @param {string} sha The SHA of the file being updated
      * @param {string} content The file content
      * @param {string} commitMessage The commit message
      */
-    async updateContent(branchName, path, sha, content, commitMessage) {
+    async updateContent(branchName, filePath, sha, content, commitMessage) {
         try {
             return await this.octokit.repos.createOrUpdateFileContents({
                 owner: this.owner,
                 repo: this.name,
                 branch: branchName,
-                path,
+                path: filePath,
                 message: commitMessage,
                 content: Buffer.from(content, 'ascii').toString('base64'),
                 sha,
             });
         } catch (err) {
-            err.message = `Error when updating content of file ${path} on branch ${branchName}: ${err.message}`;
+            err.message = `Error when updating content of file ${filePath} on branch ${branchName}: ${err.message}`;
             throw err;
         }
     }
