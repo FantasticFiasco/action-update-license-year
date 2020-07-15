07621e41091efb23e7d6f6dfc9b914ae08776046
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2020-07-15 22:04:03 +0200

    feat: support for agpl-3.0-only licenses (#52)

--- test/license.spec.js
+++ test/license.spec.js
@@ -9,51 +9,63 @@ describe('#update should', () => {
 });
 
 describe('#updateToYear should', () => {
+    test('update GNU Affero General Public License v3.0 only license from a single year into a range of years', () => {
+        const got = updateLicenseToYear(readTestFile('AGPL-3.0-only/SINGLE_YEAR'), 2002);
+        const want = readTestFile('AGPL-3.0-only/SINGLE_YEAR_EXPECTED');
+        expect(got).toBe(want);
+    });
+
+    test('update GNU Affero General Public License v3.0 only license from a range of years into a new range of years', () => {
+        const got = updateLicenseToYear(readTestFile('AGPL-3.0-only/RANGE_OF_YEARS'), 2002);
+        const want = readTestFile('AGPL-3.0-only/RANGE_OF_YEARS_EXPECTED');
+        expect(got).toBe(want);
+    });
+
     test('update Apache 2.0 license from a single year into a range of years', () => {
-        const got = updateLicenseToYear(readTestFile('apache-2.0/SINGLE_YEAR'), 2002);
-        const want = readTestFile('apache-2.0/SINGLE_YEAR_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('Apache-2.0/SINGLE_YEAR'), 2002);
+        const want = readTestFile('Apache-2.0/SINGLE_YEAR_EXPECTED');
         expect(got).toBe(want);
     });
 
     test('update Apache 2.0 license from a range of years into a new range of years', () => {
-        const got = updateLicenseToYear(readTestFile('apache-2.0/RANGE_OF_YEARS'), 2002);
-        const want = readTestFile('apache-2.0/RANGE_OF_YEARS_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('Apache-2.0/RANGE_OF_YEARS'), 2002);
+        const want = readTestFile('Apache-2.0/RANGE_OF_YEARS_EXPECTED');
         expect(got).toBe(want);
     });
 
     test('update BSD 2-clause "Simplified" license from a single year into a range of years', () => {
-        const got = updateLicenseToYear(readTestFile('bsd-2-clause/SINGLE_YEAR'), 2002);
-        const want = readTestFile('bsd-2-clause/SINGLE_YEAR_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('BSD-2-Clause/SINGLE_YEAR'), 2002);
+        const want = readTestFile('BSD-2-Clause/SINGLE_YEAR_EXPECTED');
         expect(got).toBe(want);
     });
 
     test('update BSD 2-clause "Simplified" license from a range of years into a new range of years', () => {
-        const got = updateLicenseToYear(readTestFile('bsd-2-clause/RANGE_OF_YEARS'), 2002);
-        const want = readTestFile('bsd-2-clause/RANGE_OF_YEARS_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('BSD-2-Clause/RANGE_OF_YEARS'), 2002);
+        const want = readTestFile('BSD-2-Clause/RANGE_OF_YEARS_EXPECTED');
         expect(got).toBe(want);
     });
 
     test('update BSD 3-clause "New" or "Revised" license from a single year into a range of years', () => {
-        const got = updateLicenseToYear(readTestFile('bsd-3-clause/SINGLE_YEAR'), 2002);
-        const want = readTestFile('bsd-3-clause/SINGLE_YEAR_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('BSD-3-Clause/SINGLE_YEAR'), 2002);
+        const want = readTestFile('BSD-3-Clause/SINGLE_YEAR_EXPECTED');
         expect(got).toBe(want);
     });
 
     test('update BSD 3-clause "New" or "Revised" license from a range of years into a new range of years', () => {
-        const got = updateLicenseToYear(readTestFile('bsd-3-clause/RANGE_OF_YEARS'), 2002);
-        const want = readTestFile('bsd-3-clause/RANGE_OF_YEARS_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('BSD-3-Clause/RANGE_OF_YEARS'), 2002);
+        const want = readTestFile('BSD-3-Clause/RANGE_OF_YEARS_EXPECTED');
         expect(got).toBe(want);
     });
 
     test('update MIT license from a single year into a range of years', () => {
-        const got = updateLicenseToYear(readTestFile('mit/SINGLE_YEAR'), 2002);
-        const want = readTestFile('mit/SINGLE_YEAR_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('MIT/SINGLE_YEAR'), 2002);
+        const want = readTestFile('MIT/SINGLE_YEAR_EXPECTED');
         expect(got).toBe(want);
     });
 
     test('update MIT license from a range of years into a new range of years', () => {
-        const got = updateLicenseToYear(readTestFile('mit/RANGE_OF_YEARS'), 2002);
-        const want = readTestFile('mit/RANGE_OF_YEARS_EXPECTED');
+        const got = updateLicenseToYear(readTestFile('MIT/RANGE_OF_YEARS'), 2002);
+        const want = readTestFile('MIT/RANGE_OF_YEARS_EXPECTED');
         expect(got).toBe(want);
     });
 
