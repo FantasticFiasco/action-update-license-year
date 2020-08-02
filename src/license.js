a2892fae56bab710ac06b5c6e03e5f1dd371e99e
FantasticFiasco <mattias.kindborg@gmail.com>
2020-08-02 08:19:14 +0200

    wip

--- src/license.js
+++ src/license.js
@@ -2,29 +2,55 @@
 // - GNU Affero General Public License v3.0 only (AGPL-3.0-only)
 // prettier-ignore
 const AGPL_3_ONLY_YEAR_RANGE = new RegExp(
-    '(?<=copyright\\s+\\(c\\)\\s+)' +           // 'Copyright (C) '             positive lookbehind
-    '(?<from>\\d{4})' +                         // 'YYYY'                       group named 'from'
-    '-\\d{4}' +                                 // '-YYYY'
-    '(?!\\s+free\\s+software\\s+foundation)',   // ' Free Software Foundation'  negative lookahead
-    'im'
+    '(?<=copyright\\s+\\(c\\)\\s+)'           +  // 'Copyright (C) '             positive lookbehind
+    '(?<from>\\d{4})'                         +  // 'YYYY'                       group named 'from'
+    '-\\d{4}'                                 +  // '-YYYY'
+    '(?!\\s+free\\s+software\\s+foundation)',    // ' Free Software Foundation'  negative lookahead
+    'im'                                         // Multi-line/Insensitive
 );
-const AGPL_3_ONLY_SINGLE_YEAR = /(?<=copyright\s+\(c\)\s+)(?<from>\d{4})(?!\s+free software foundation)/im;
+// prettier-ignore
+const AGPL_3_ONLY_SINGLE_YEAR = new RegExp(
+    '(?<=copyright\\s+\\(c\\)\\s+)'           +  // 'Copyright (C) '             positive lookbehind
+    '(?<from>\\d{4})'                         +  // 'YYYY'                       group named 'from'
+    '(?!\\s+free software foundation)',          // ' Free Software Foundation'  negative lookahead
+    'im'                                         // Multi-line/Insensitive
+);
 
 // Regular expressions capable of transforming the following license files:
 // - Apache 2.0 (Apache-2.0)
-const APACHE_2_YEAR_RANGE = /(?<=copyright\s+)(?<from>\d{4})-\d{4}/im;
-const APACHE_2_SINGLE_YEAR = /(?<=copyright\s+)(?<from>\d{4})/im;
+// - MIT (MIT)
+// prettier-ignore
+const APACHE_2_MIT_YEAR_RANGE = new RegExp(
+    '(?<=copyright\\s+)'                      +  // 'Copyright '                 positive lookbehind
+    '(?<from>\\d{4})'                         +  // 'YYYY'                       group named 'from'
+    '-\\d{4}',                                   // '-YYYY'
+    'im'                                         // Multi-line/Insensitive
+);
+// prettier-ignore
+const APACHE_2_MIT_SINGLE_YEAR = new RegExp(
+    '(?<=copyright\\s+)'                      +  // 'Copyright '                 positive lookbehind
+    '(?<from>\\d{4})',                           // 'YYYY'                       group named 'from'
+    'im'                                         // Multi-line/Insensitive
+);
 
 // Regular expressions capable of transforming the following license files:
 // - BSD 2-clause "Simplified" (BSD-2-Clause)
 // - BSD 3-clause "New" or "Revised" (BSD-3-Clause)
-const BSD_YEAR_RANGE = /(?<=copyright\s+\(c\)\s+)(?<from>\d{4})-\d{4}(?=,)/im;
-const BSD_SINGLE_YEAR = /(?<=copyright\s+\(c\)\s+)(?<from>\d{4})(?=,)/im;
-
-// Regular expressions capable of transforming the following license files:
-// - MIT (MIT)
-const MIT_YEAR_RANGE = /(?<=copyright\s+\(c\)\s+)(?<from>\d{4})-\d{4}/im;
-const MIT_SINGLE_YEAR = /(?<=copyright\s+\(c\)\s+)(?<from>\d{4})/im;
+// prettier-ignore
+const BSD_YEAR_RANGE = new RegExp(
+    '(?<=copyright\\s+\\(c\\)\\s+)'           +  // 'Copyright (c) '             positive lookbehind
+    '(?<from>\\d{4})'                         +  // 'YYYY'                       group named 'from'
+    '-\\d{4}'                                 +  // '-YYYY'
+    '(?=,)',                                     // ','                          positive lookahead
+    'im'                                         // Multi-line/Insensitive
+)
+// prettier-ignore
+const BSD_SINGLE_YEAR = new RegExp(
+    '(?<=copyright\\s+\\(c\\)\\s+)'           +  // 'Copyright (c) '             positive lookbehind
+    '(?<from>\\d{4})'                         +  // 'YYYY'                       group named 'from'
+    '(?=,)',                                     // ','                          positive lookahead
+    'im'                                         // Multi-line/Insensitive
+);
 
 /**
  * @typedef LicenseTransform
@@ -34,12 +60,10 @@ const MIT_SINGLE_YEAR = /(?<=copyright\s+\(c\)\s+)(?<from>\d{4})/im;
 const LICESE_TRANSFORMS = [
     { name: 'AGPL-3.0-only', transform: AGPL_3_ONLY_YEAR_RANGE },
     { name: 'AGPL-3.0-only', transform: AGPL_3_ONLY_SINGLE_YEAR },
-    { name: 'Apache-2.0', transform: APACHE_2_YEAR_RANGE },
-    { name: 'Apache-2.0', transform: APACHE_2_SINGLE_YEAR },
-    { name: 'BSD-2-Clause/BSD-3-Clause', transform: BSD_YEAR_RANGE },
-    { name: 'BSD-2-Clause/BSD-3-Clause', transform: BSD_SINGLE_YEAR },
-    { name: 'MIT', transform: MIT_YEAR_RANGE },
-    { name: 'MIT', transform: MIT_SINGLE_YEAR },
+    { name: 'Apache-2.0', transform: APACHE_2_MIT_YEAR_RANGE },
+    { name: 'Apache-2.0', transform: APACHE_2_MIT_SINGLE_YEAR },
+    { name: 'BSD-2-Clause/BSD-3-Clause/MIT', transform: BSD_YEAR_RANGE },
+    { name: 'BSD-2-Clause/BSD-3-Clause/MIT', transform: BSD_SINGLE_YEAR },
 ];
 
 /**
