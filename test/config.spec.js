d60064524c9a461ac720edca1f6deb009c7d2269
FantasticFiasco <mattias.kindborg@gmail.com>
2020-08-02 13:32:15 +0200

    wip

--- test/config.spec.js
+++ test/config.spec.js
@@ -38,100 +38,116 @@ describe('#parseConfig should', () => {
     });
 
     describe('given configuration', () => {
-        test('return token', () => {
-            mockCore.getInput.mockReturnValueOnce('some token');
-            const { token } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('token', { required: true });
-            expect(token).toBe('some token');
+        describe('with token', () => {
+            test('returns it', () => {
+                mockCore.getInput.mockReturnValueOnce('some token');
+                const { token } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('token', { required: true });
+                expect(token).toBe('some token');
+            });
         });
 
-        test('return branch name given no variable', () => {
-            getInputSkip(1).mockReturnValueOnce('some-branch-name');
-            const { branchName } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('branchName');
-            expect(branchName).toBe('some-branch-name');
+        describe('with branch name', () => {
+            test('returns it given no variable', () => {
+                getInputSkip(1).mockReturnValueOnce('some-branch-name');
+                const { branchName } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('branchName');
+                expect(branchName).toBe('some-branch-name');
+            });
+
+            test('returns it given "currentYear" variable', () => {
+                getInputSkip(1).mockReturnValueOnce('some-branch-name-{{currentYear}}');
+                const { branchName } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('branchName');
+                expect(branchName).toBe(`some-branch-name-${CURRENT_YEAR}`);
+            });
+
+            test('returns it given "currentYear" variable with leading and trailing spaces', () => {
+                getInputSkip(1).mockReturnValueOnce('some-branch-name-{{ currentYear }}');
+                const { branchName } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('branchName');
+                expect(branchName).toBe(`some-branch-name-${CURRENT_YEAR}`);
+            });
+
+            test('throws error given invalid variable', () => {
+                getInputSkip(1).mockReturnValueOnce('some-branch-name-{{invalidVariableName}}');
+                const fn = () => parseConfig();
+                expect(fn).toThrow();
+            });
+
+            test('throws error given invalid variable with leading and trailing spaces', () => {
+                getInputSkip(1).mockReturnValueOnce('some-branch-name-{{ invalidVariableName }}');
+                const fn = () => parseConfig();
+                expect(fn).toThrow();
+            });
         });
 
-        test('return branch name given variable', () => {
-            getInputSkip(1).mockReturnValueOnce('some-branch-name-{{currentYear}}');
-            const { branchName } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('branchName');
-            expect(branchName).toBe(`some-branch-name-${CURRENT_YEAR}`);
+        describe('with commit title', () => {
+            test('returns it', () => {
+                getInputSkip(2).mockReturnValueOnce('some commit title');
+                const { commitTitle } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('commitTitle');
+                expect(commitTitle).toBe('some commit title');
+            });
         });
 
-        test('return branch name given variable with leading and trailing spaces', () => {
-            getInputSkip(1).mockReturnValueOnce('some-branch-name-{{ currentYear }}');
-            const { branchName } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('branchName');
-            expect(branchName).toBe(`some-branch-name-${CURRENT_YEAR}`);
+        describe('with commit body', () => {
+            test('returns it', () => {
+                getInputSkip(3).mockReturnValueOnce('some commit body');
+                const { commitBody } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('commitBody');
+                expect(commitBody).toBe('some commit body');
+            });
         });
 
-        test('throw error given invalid branch name variable', () => {
-            getInputSkip(1).mockReturnValueOnce('some-branch-name-{{invalidVariableName}}');
-            const fn = () => parseConfig();
-            expect(fn).toThrow();
+        describe('with pull request title', () => {
+            test('returns it', () => {
+                getInputSkip(4).mockReturnValueOnce('some pull request title');
+                const { pullRequestTitle } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('prTitle');
+                expect(pullRequestTitle).toBe('some pull request title');
+            });
         });
 
-        test('throw error given invalid branch name variable with leading and trailing spaces', () => {
-            getInputSkip(1).mockReturnValueOnce('some-branch-name-{{ invalidVariableName }}');
-            const fn = () => parseConfig();
-            expect(fn).toThrow();
+        describe('with pull request body', () => {
+            test('returns it', () => {
+                getInputSkip(5).mockReturnValueOnce('some pull request body');
+                const { pullRequestBody } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('prBody');
+                expect(pullRequestBody).toBe('some pull request body');
+            });
         });
 
-        test('return commit title', () => {
-            getInputSkip(2).mockReturnValueOnce('some commit title');
-            const { commitTitle } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('commitTitle');
-            expect(commitTitle).toBe('some commit title');
+        describe('with assignees', () => {
+            test('returns them', () => {
+                getInputSkip(6).mockReturnValueOnce('assignee1,assignee2,assignee3');
+                const { assignees } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('assignees');
+                expect(assignees).toStrictEqual(['assignee1', 'assignee2', 'assignee3']);
+            });
+
+            test('return them given leading and trailing spaces', () => {
+                getInputSkip(6).mockReturnValueOnce(' assignee1 , assignee2 , assignee3 ');
+                const { assignees } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('assignees');
+                expect(assignees).toStrictEqual(['assignee1', 'assignee2', 'assignee3']);
+            });
         });
 
-        test('return commit body', () => {
-            getInputSkip(3).mockReturnValueOnce('some commit body');
-            const { commitBody } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('commitBody');
-            expect(commitBody).toBe('some commit body');
-        });
+        describe('with labels', () => {
+            test('returns them', () => {
+                getInputSkip(7).mockReturnValueOnce('some label 1,some label 2,some label 3');
+                const { labels } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('labels');
+                expect(labels).toStrictEqual(['some label 1', 'some label 2', 'some label 3']);
+            });
 
-        test('return pull request title', () => {
-            getInputSkip(4).mockReturnValueOnce('some pull request title');
-            const { pullRequestTitle } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('prTitle');
-            expect(pullRequestTitle).toBe('some pull request title');
-        });
-
-        test('return pull request body', () => {
-            getInputSkip(5).mockReturnValueOnce('some pull request body');
-            const { pullRequestBody } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('prBody');
-            expect(pullRequestBody).toBe('some pull request body');
-        });
-
-        test('return assignees', () => {
-            getInputSkip(6).mockReturnValueOnce('assignee1,assignee2,assignee3');
-            const { assignees } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('assignees');
-            expect(assignees).toStrictEqual(['assignee1', 'assignee2', 'assignee3']);
-        });
-
-        test('return assignees given leading and trailing spaces', () => {
-            getInputSkip(6).mockReturnValueOnce(' assignee1 , assignee2 , assignee3 ');
-            const { assignees } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('assignees');
-            expect(assignees).toStrictEqual(['assignee1', 'assignee2', 'assignee3']);
-        });
-
-        test('return labels', () => {
-            getInputSkip(7).mockReturnValueOnce('some label 1,some label 2,some label 3');
-            const { labels } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('labels');
-            expect(labels).toStrictEqual(['some label 1', 'some label 2', 'some label 3']);
-        });
-
-        test('return labels given leading and trailing spaces', () => {
-            getInputSkip(7).mockReturnValueOnce(' some label 1 , some label 2 , some label 3 ');
-            const { labels } = parseConfig();
-            expect(mockCore.getInput).toBeCalledWith('labels');
-            expect(labels).toStrictEqual(['some label 1', 'some label 2', 'some label 3']);
+            test('returns them given leading and trailing spaces', () => {
+                getInputSkip(7).mockReturnValueOnce(' some label 1 , some label 2 , some label 3 ');
+                const { labels } = parseConfig();
+                expect(mockCore.getInput).toBeCalledWith('labels');
+                expect(labels).toStrictEqual(['some label 1', 'some label 2', 'some label 3']);
+            });
         });
     });
 });
