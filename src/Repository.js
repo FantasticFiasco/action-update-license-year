932a22402576c0fc8ab2f31092390372efa458b2
FantasticFiasco <mattias.kindborg@gmail.com>
2020-12-27 21:45:08 +0100

    wip

--- src/Repository.js
+++ src/Repository.js
@@ -107,9 +107,9 @@ class Repository {
      */
     async commit(message) {
         try {
-            await exec('git config user.name "GitHub Actions"');
-            await exec('git config user.email "actions@.com"');
-            
+            // await exec('git config user.name "GitHub Actions"');
+            // await exec('git config user.email "actions@.com"');
+
             await exec(`git commit -m "${message}"`);
         } catch (err) {
             err.message = `Error committing staged files: ${err.message}`;
