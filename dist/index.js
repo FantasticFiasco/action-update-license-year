7cafe180cc58b2abcb3ae989236aee860a5107e7
FantasticFiasco <mattias.kindborg@gmail.com>
2020-12-27 22:59:45 +0100

    wip

--- dist/index.js
+++ dist/index.js
@@ -2675,9 +2675,6 @@ class Repository {
             const { stdout } = await exec(`git remote show origin | grep 'HEAD branch' | cut -d ' ' -f5`);
             const defaultBranch = stdout.toString().trim();
 
-            // TODO: Remove logging
-            info(`default branch: "${defaultBranch}"`);
-
             return await this._octokit.pulls.create({
                 owner: this._owner,
                 repo: this._name,
