078f91eedf92b333594532072c729ac0df94ecb1
FantasticFiasco <mattias.kindborg@gmail.com>
2020-12-31 10:09:27 +0100

    wip

--- src/action-update-license-year.js
+++ src/action-update-license-year.js
@@ -95,7 +95,7 @@ const run = async () => {
  * @param {string} text
  */
 const singleLine = (text) => {
-    return text.replace(/\\n/g, '\\n');
+    return text.replace(/\n/g, '\\n');
 };
 
 module.exports = {
