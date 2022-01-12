4e15a9210d970cffc089e3b2034ad360ef661392
FantasticFiasco <mattias@kindb.org>
2022-01-12 08:13:37 +0100

    wip

---
+++ test/os/temp.test.js
@@ -0,0 +1,20 @@
+const { tempDir, tempFile } = require('../../src/os/temp');
+const { dirname, basename } = require('path');
+const { existsSync } = require('fs');
+
+describe('#tempDir should', () => {
+    test('return an existing directory', () => {
+        const got = tempDir();
+        expect(got).toBeDefined();
+        expect(existsSync(got)).toBeTruthy();
+    });
+});
+
+describe('#tempFile should', () => {
+    test('return a file name in the temp directory', () => {
+        const got = tempFile('test.txt');
+        expect(got).toBeDefined();
+        expect(dirname(got)).toStrictEqual(tempDir());
+        expect(basename(got)).toStrictEqual('test.txt');
+    });
+});
